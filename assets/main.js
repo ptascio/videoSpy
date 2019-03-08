427571445a512281702cb0ba87da0c37eb1f79d7
Peter Tascio <peter.tascio@gmail.com>
2019-03-08 08:56:01 -0500

    Play with localstorage functions

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,21 @@
-function getArtistName(){
-  console.log($("#artistName").val());
-}
-
-$("#button").on("click", getArtistName);
-
-const instance = axios.create({
-  headers: {
-    get: {
-      "X-RapidAPI-Key": ""
-    }
-  }
-});
-
-instance.get("https://deezerdevs-deezer.p.rapidapi.com/search?q=thebeatles")
-  .then(function (result) {
-    console.log(result.data);
-  });
-
-  // function getCookie(c_name) {
-  //     return localStorage.getItem(c_name);
-  //
+// function getArtistName(){
+//   console.log($("#artistName").val());
+// }
+//
+// $("#button").on("click", getArtistName);
+//
+// const instance = axios.create({
+//   headers: {
+//     get: {
+//       "X-RapidAPI-Key": ""
+//     }
+//   }
+// });
+//
+// instance.get("https://deezerdevs-deezer.p.rapidapi.com/search?q=thebeatles")
+//   .then(function (result) {
+//     console.log(result.data);
+//   });
 
   function setCookie(cName, value, expiredays) {
       return localStorage.setItem(cName, value);
@@ -29,11 +25,15 @@ instance.get("https://deezerdevs-deezer.p.rapidapi.com/search?q=thebeatles")
     console.log(localStorage.getItem(cName));
 }
 
-function removeCookie(cName){
-  localStorage.removeItem(cName);
-  getCookie(cName);
+function removeCookie(e){
+  console.log(e.data.param);
+  localStorage.removeItem(e.data.param);
 }
 
-  removeCookie("Pete's cookie");
+if (localStorage.getItem("Pete's cookie") === null){
+  setCookie("Pete's cookie", "This is my cookie");
+}else{
+  getCookie("Pete's cookie");
+}
 
-  //GEThttps://api.vimeo.com/videos
+$("#removeCookie").on("click", {param: "Pete's cookie"}, removeCookie);
