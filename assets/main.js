80278d0ca4a2c630651519f41299fa41c2954e82
Peter Tascio <peter.tascio@gmail.com>
2019-03-08 08:26:59 -0500

    Add Deezer api call

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,16 @@
-// 17143248dab3d530e411d5020865c870
-// const instance = axios.create({
-//   headers: {
-//     get: {
-//       "Authorization": ""
-//     }
-//   }
-// });
-//
-// instance.get("https://api.vimeo.com/videos")
-//   .then(function (result) {
-//     console.log(result.data);
-//   });
+
+
+const instance = axios.create({
+  headers: {
+    get: {
+      "X-RapidAPI-Key": ""
+    }
+  }
+});
+
+instance.get("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem")
+  .then(function (result) {
+    console.log(result.data);
+  });
 
   //GEThttps://api.vimeo.com/videos
-  console.log(this);
