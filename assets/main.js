a439be4720df90c67577c898fbc5d5b7fb973d78
Peter Tascio <peter.tascio@gmail.com>
2019-03-07 22:05:36 -0500

    Not much changed

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
-alert("hi");
+// 17143248dab3d530e411d5020865c870
+// const instance = axios.create({
+//   headers: {
+//     get: {
+//       "Authorization": ""
+//     }
+//   }
+// });
+//
+// instance.get("https://api.vimeo.com/videos")
+//   .then(function (result) {
+//     console.log(result.data);
+//   });
+
+  //GEThttps://api.vimeo.com/videos
+  console.log(this);
