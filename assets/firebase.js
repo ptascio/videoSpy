35b38f93ec721773313442eca3657ec65aee5c90
Peter Tascio <peter.tascio@gmail.com>
2019-03-09 09:58:51 -0500

    Add firebase file

---
+++ assets/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "",
+    authDomain: "videospy-bc870.firebaseapp.com",
+    databaseURL: "https://videospy-bc870.firebaseio.com",
+    projectId: "videospy-bc870",
+    storageBucket: "videospy-bc870.appspot.com",
+    messagingSenderId: "187551112740"
+  };
+  firebase.initializeApp(config);
