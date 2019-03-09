b690a2979daa204420e6d67f6493a0f1a6283584
Peter Tascio <peter.tascio@gmail.com>
2019-03-09 10:03:11 -0500

    Delete firebase.js

--- assets/firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "",
-    authDomain: "videospy-bc870.firebaseapp.com",
-    databaseURL: "https://videospy-bc870.firebaseio.com",
-    projectId: "videospy-bc870",
-    storageBucket: "videospy-bc870.appspot.com",
-    messagingSenderId: "187551112740"
-  };
-  firebase.initializeApp(config);
